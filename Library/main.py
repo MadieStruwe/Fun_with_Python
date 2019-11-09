c1acdcdf1d9689bcb206148b66f83c7e29381904
MadieStruwe <struwe.madelaine@gmail.com>
2019-11-08 20:17:13 -0700

    new project
    new project of Library Managment System Started. Inital files created

---
+++ Library/main.py
@@ -0,0 +1,13 @@
+'''
+Library Managment System
+Date Created: 11-8-19
+Author: Madie Struwe
+
+This project will be a Library Managment System
+Will be able to
+-Add Books
+-Remove Books
+-Add Members
+-Allocate Books to Members
+-Deallocate Books to Members
+'''
